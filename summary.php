dda3f7508d3105fa2a813af0b18ecffd5576568e
Deepak Puthraya <deepakputhraya@gmail.com>
2014-07-04 19:07:06 +0530

    Update summary.php

--- summary.php
+++ summary.php
@@ -9,9 +9,6 @@
         <link rel="stylesheet" href="css/template.css" type="text/css" />
         <link rel="stylesheet" href="//code.jquery.com/ui/1.11.0/themes/smoothness/jquery-ui.css">
 
-
-
-
         <script src="js/jquery-1.10.2.js"></script>
         <script src="//code.jquery.com/ui/1.11.0/jquery-ui.js"></script>
         <script src="js/highcharts.js"></script>
@@ -36,54 +33,6 @@
         <div class="summaryeach" id="daily_graph">Daily Analysis</div>
         <div class="summaryeach" id="weekly_graph">Weekly Analysis</div>
         <div class="summaryeach" id="monthly_graph">Monthly  Analysis</div>
-
-
-        <div class="datepicker" id="datechoose">
-            Date1: <input type="text" name="datepicker" id="datepicker"  />
-
-        </div>
-        <script>
-       
-
-            $(function() {
-                $("#datepicker").datepicker({
-                    changeMonth: true,
-                    changeYear: true,
-                    dateFormat:"yy/mm/dd"
-                });
-            });
-        </script>
-
-
-
-
-        <div class="datepicker" id="monthchoose">
-            Date2: <input type="text" id="monthpicker">
-        </div>
-        <script>
-            $(function() {
-                $("#monthpicker").datepicker({
-                    changeMonth: true,
-                    changeYear: true,
-                    dateFormat:"yy/mm/dd"
-                });
-            });
-        </script>
-
-
-
-        <div class="datepicker" id="yearchoose">
-            Date3: <input type="text" id="yearpicker">
-        </div>
-        <script>
-            $(function() {
-                $("#yearpicker").datepicker({
-                    changeMonth: false,
-                    changeYear: true,
-                    dateFormat:"yy/mm/dd"
-                });
-            });
-        </script>
 
 
         <div class="datepicker" id="datechoose">
